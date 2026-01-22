9e5819a42934b473c8b9f6e17f3b20496ba5644f
Pedro <pedroalbaladejolopez@gmail.com>
2026-01-22 09:48:13 +0100

    Proyecto Sprinboot

---
+++ src/main/java/es/mde/et/madoc/panacea_backend/PanaceaBackendApplication.java
@@ -0,0 +1,13 @@
+package es.mde.et.madoc.panacea_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanaceaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanaceaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/mde/et/madoc/panacea_backend/PanaceaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mde.et.madoc.panacea_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanaceaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
